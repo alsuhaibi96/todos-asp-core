86f4ed176dde492677c039269538cdc870fcab65
ajeer-alsuhaibi <a.alsuhaibi@iajeer.com>
2025-09-24 13:01:52 +0300

    feat: custom and system middlewares

--- Program.cs
+++ Program.cs
@@ -1,10 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Rewrite;
 
 var builder = WebApplication.CreateBuilder(args);
 
 
 var app = builder.Build();
+
+app.UseRewriter(new RewriteOptions().AddRedirect("tasks/(.*)", "todos/$1"));
+
+app.Use(async (context, next) =>
+{
+    Console.WriteLine($"[{context.Request.Method}  {context.Request.Path} {DateTime.UtcNow} started]");
+    await next();
+    Console.WriteLine($"[{context.Request.Method}  {context.Request.Path} {DateTime.UtcNow} finshed]");
+});
 
 
 //todos
@@ -15,7 +25,6 @@ app.MapGet("/todos", () => Results.Ok(todos));
 app.MapGet("/todos/{id}", Results<Ok<Todo>, NotFound> (int id) =>
 {
     var targetToDo = todos.SingleOrDefault(t => id == t.Id);
-
     return targetToDo is null ? TypedResults.NotFound()
     : TypedResults.Ok(targetToDo);
 
--- obj/Debug/net8.0/myFirstDotNetApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myFirstDotNetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myFirstDotNetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54faf8349973bb5304d08fdddfa25b12fbcadd43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4249afb5caa61d947a787985748bf111bdd43129")]
 [assembly: System.Reflection.AssemblyProductAttribute("myFirstDotNetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myFirstDotNetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
